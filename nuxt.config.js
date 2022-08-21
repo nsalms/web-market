9b7f9dd74be9d1f226297c7121952059501213e8
Nikita Salms <salms.nikita@yandex.ru>
2022-08-21 18:17:37 +0300

    Generated files

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,6 +1,9 @@
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
+  router: {
+    base: '/web-market/'
+  },
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
